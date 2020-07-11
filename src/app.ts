6fd6ac79615cf348ec15e0dc9467bd388bc3c862
Vishal Biswas <vshlbiswas@ymail.com>
2020-07-11 09:47:55 +0530

    add amazon ssl signature verification

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,23 @@ import express from "express";
 import compression from "compression";
 import bodyParser from "body-parser";
 import path from "path";
+import { ExpressAdapter } from "ask-sdk-express-adapter";
 
 // Controllers (route handlers)
-import { handler } from "./skill";
+import { skill } from "./skill";
+import { prod } from "./util/secrets";
 
 // Create Express server
 const app = express();
+const adapter = new ExpressAdapter(skill, prod, true);
 
 // Express configuration
+app.post("/", adapter.getRequestHandlers()); // needs to be done before adding parsers
 app.set("port", process.env.PORT || 3000);
 app.use(compression());
 app.use(bodyParser.json());
 app.use(
     express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
-
-app.post("/", handler);
 
 export default app;
--- src/skill/index.ts
+++ src/skill/index.ts
@@ -1,23 +1,12 @@
-import { Request, Response } from "express";
 import * as Alexa from "ask-sdk-core";
 import { intents } from "./intents";
 import { errors } from "./errors";
-import { RequestEnvelope } from "ask-sdk-model";
 import { SKILL_ID } from "../util/secrets";
 import { sqlAdapter } from "./lib/persistence-adapters";
 
-const alexa = Alexa.SkillBuilders.custom()
+export const skill = Alexa.SkillBuilders.custom()
     .withSkillId(SKILL_ID)
     .addRequestHandlers(...intents)
     .addErrorHandlers(...errors)
     .withPersistenceAdapter(sqlAdapter)
     .create();
-
-export const handler = async (req: Request, res: Response) => {
-    alexa.invoke(req.body as RequestEnvelope, null)
-        .then(result => {
-            return res.status(200).json(result);
-        }).catch(err => {
-            return res.status(500).send(err);
-        });
-};
